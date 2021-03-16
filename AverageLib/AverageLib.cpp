ba16ebf3bfdec48aa2a7e0fa3b9b2163ee221d5f
Michał Laskowski <01158164@pw.edu.pl>
2021-03-16 01:55:39 +0100

    Second try of pushing files without deleting everything else...

---
+++ AverageLib/AverageLib.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <array>
+#include <string>
+#include "Average.h"
+using namespace std;
+
+double average(array<array<int, maxColumns>, maxRows>& array, size_t rows, size_t columns)
+{
+    double sum_all = 0;
+    int count_elements = 0;
+    for (size_t row = 0; row < rows; row++)
+    {
+        for (size_t column = 0; column < columns; column++)
+        {
+            int inner_sum = 0;
+            int element = array[row][column];
+            string str_element = to_string(element);
+            if (str_element.length() > 1)
+            {
+                for (int k = str_element.length() - 2; k >= 0; k -= 2)
+                {
+                    inner_sum += (int)str_element[k] - 48;
+                }
+                if (inner_sum % 2 == 0)
+                {
+                    sum_all += element;
+                    count_elements++;
+                }
+            }
+
+        }
+    }
+    if (sum_all == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return sum_all / count_elements;
+    }
+}
