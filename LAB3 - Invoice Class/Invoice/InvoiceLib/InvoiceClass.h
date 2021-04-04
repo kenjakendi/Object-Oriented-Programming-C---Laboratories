b6cd1d880f55454a1607aa4cdc0cceb2b8d1835b
Michał Damian Laskowski <01158164@pw.edu.pl>
2021-04-05 00:30:37 +0200

    Added comments in headers

--- LAB3 - Invoice Class/Invoice/InvoiceLib/InvoiceClass.h
+++ LAB3 - Invoice Class/Invoice/InvoiceLib/InvoiceClass.h
@@ -7,22 +7,29 @@
 class Invoice
 {
 private:
-	std::string name;
-	std::string adress;
-	std::vector <Item> items;
-	bool sameData(Invoice const& invoice) const;
+	std::string name; // Name of person on invoice
+	std::string adress; // Adress of peron on invoice
+	std::vector <Item> items; // List of items on invoice
+	bool sameData(Invoice const& invoice) const; // Checks if data on invoice is the same
 public:
+	// Constructor
 	Invoice(std::string name, std::string adress, std::vector <Item> items = {});
-	std::string getName() const;
+
+	// Returns name, adress or list of items
+	std::string getName() const; 
 	std::string getAdress() const;
 	std::vector <Item> getItems() const;
+
+	// Enables setting new name, adress or new list of items
 	void setName(std::string const name);
 	void setAdress(std::string const adress);
 	void setItems(std::vector <Item> const items);
 
+	//Adds or removes an item from the list
 	void addItem(Item const& item);
 	void removeItem(Item const& item);
 
+	//Operators
 	Invoice operator+(Invoice const& invoice) const;
 	Invoice operator-(Invoice const& invoice) const;
 	Invoice operator+(Item const& item) const;
@@ -33,6 +40,7 @@ public:
 	void operator+=(Item const& item);
 	void operator-=(Item const& item);
 
+	// In and Out
 	friend std::ostream& operator<< (std::ostream& os, const Invoice& invoice);
 	friend std::istream& operator>> (std::istream& is, Invoice& invoice);
 };
--- LAB3 - Invoice Class/Invoice/InvoiceLib/ItemClass.h
+++ LAB3 - Invoice Class/Invoice/InvoiceLib/ItemClass.h
@@ -4,12 +4,18 @@
 class Item
 {
 private:
-	std::string name;
+	std::string name; // Name of item
 public:
+	// Constructor
 	Item(std::string name);
+
+	// Returns name of item
 	std::string getName() const;
+
+	// Enables setting name of item
 	void setName(std::string name);
 
+	//Operators
 	bool operator==(Item const& item) const;
 
 	friend std::ostream& operator<<(std::ostream& os, const Item& item);
