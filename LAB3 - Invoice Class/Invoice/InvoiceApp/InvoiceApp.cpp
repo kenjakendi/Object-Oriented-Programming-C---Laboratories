aeb1f9d29490c37da9583196f28c201e56e0652c
Michał Damian Laskowski <01158164@pw.edu.pl>
2021-04-04 14:18:06 +0200

    Added Invoice console app

---
+++ LAB3 - Invoice Class/Invoice/InvoiceApp/InvoiceApp.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "../InvoiceLib/InvoiceClass.h"
+#include "../InvoiceLib/InvoiceClass.cpp"
+
+using namespace std;
+
+int main()
+{
+    Invoice inv = Invoice("Anna", "Warsaw", { "Apple","Banana","Mango" });
+    cout << inv;
+    Invoice inv2 = Invoice("Anna", "Warsaw");
+    cin >> inv2;
+    cout << inv2;
+
+}
