a6055f0c54d8d8bf98519077f4ded1f445e0c525
Michał Laskowski <01158164@pw.edu.pl>
2021-04-19 19:56:11 +0200

    Prepared files for lab 4

---
+++ LAB4 - Drawing/DrawingLib/DrawingUnitTest/DrawingUnitTest.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DrawingUnitTest
+{
+	TEST_CLASS(DrawingUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
